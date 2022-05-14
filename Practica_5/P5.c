bff04c3818d93aa09acf5d759cec1049a43ddece
Ang-Gar <angelel.garciaia@gmail.com>
2022-05-14 17:03:53 -0500

    practica 5

--- Practica_5/P5.c
+++ Practica_5/P5.c
@@ -40,6 +40,7 @@ void analisisLexico(char*, struct stat);
 void encontrarTipo(char*, struct stat);
 int validar(char*, struct stat, int);
 void contarIden(char*, char*, struct stat, int);
+void contarRes(char*, char*, struct stat, int);
 
 //*****************************************************************
 //PROGRAMA PRINCIPAL 
@@ -67,6 +68,7 @@ int main(int argc, char *argv[]) {
 	int k;
 	float tamor;
 	float tamcom;
+		char reser[74][17] = {{"asm\0"},{"auto\0"},{"bool\0"},{"catch\0"},{"compl\0"},{"continue\0"},{"do\0"},{"else\0"},{"export\0"},{"float\0"},{"goto\0"},{"int\0"},{"namespace\0"},{"or_eq\0"},{"public\0"},{"return\0"},{"sizeof\0"},{"struct\0"},{"this\0"},{"try\0"},{"typename\0"},{"not_eq\0"},{"using\0"},{"volatile\0"},{"and\0"},{"bitand\0"},{"break\0"},{"char\0"},{"const\0"},{"default\0"},{"double\0"},{"enum\0"},{"extern\0"},{"for\0"},{"if\0"},{"long\0"},{"new\0"},{"operator\0"},{"private\0"},{"register\0"},{"short\0"},{"static\0"},{"switch\0"},{"throw\0"},{"typedef\0"},{"union\0"},{"virtual\0"},{"wchar_t\0"},{"xor\0"},{"and_eq\0"},{"bitor\0"},{"case\0"},{"class\0"},{"const_cast\0"},{"delete\0"},{"dynamic_cast\0"},{"explicit\0"},{"false\0"},{"friend\0"},{"inline\0"},{"mutable\0"},{"not\0"},{"or\0"},{"protected\0"},{"reinterpret_cast\0"},{"signed\0"},{"static_cast\0"},{"template\0"},{"true\0"},{"typeid\0"},{"unsigned\0"},{"void\0"},{"xor_eq\0"},{"while\0"}};
 
 	nombreArchivo = argv[1];
 	
@@ -92,7 +94,12 @@ int main(int argc, char *argv[]) {
 	printf("\n\n\n");
 	printf("   Identificadores\n\n");
 
-	encontrarTipo(arreglo,sb);	
+	encontrarTipo(arreglo,sb);
+
+	printf("\n\n\n Reservadas\n");
+	for(i = 0; i<74; i++){
+		contarRes(arreglo,reser[i],sb,0);
+	}	
 	
 	return EXIT_SUCCESS;
 }
@@ -315,7 +322,7 @@ void contarIden(char *arreglo, char *identificador, struct stat sb, int pos){
 	for(i = pos; i<sb.st_size;i++){
 		//recorremos
 		if(lon == strlen(identificador)){ // Son iguales por su longitud
-		    if(arreglo[i+1] == '	' || arreglo[i+1] == ' ' || arreglo[i+1] == '\n' || arreglo[i+1] == '=' || arreglo[i+1] == '>' || arreglo[i+1] == '<' || arreglo[i+1] == '*' || arreglo[i+1] == '+' || arreglo[i+1] == '-'|| arreglo[i+1] == '['|| arreglo[i+1] == ')'|| arreglo[i+1] == ';'|| arreglo[i+1] == ',') // revisamos el siguiente valor
+		    if(arreglo[i] == '	' || arreglo[i] == ' ' || arreglo[i] == '\n' || arreglo[i] == '=' || arreglo[i] == '>' || arreglo[i] == '<' || arreglo[i] == '*' || arreglo[i] == '+' || arreglo[i] == '-'|| arreglo[i] == '{'|| arreglo[i] == '['|| arreglo[i] == ']'|| arreglo[i] == ')'|| arreglo[i] == ';'|| arreglo[i] == ',') // revisamos el siguiente valor
 		        cont++;
 		    else lon = 0; //reseteamos la longitud de la palabra
 		} else{
@@ -415,4 +422,30 @@ void encontrarTipo(char *arreglo, struct stat sb){
 			band = 1;
 		else band = 0;
 	}
+}
+
+
+void contarRes(char *arreglo, char *res, struct stat sb, int pos){
+	// variables
+	int i;
+	int cont;
+	int lon;
+
+	lon = 0;
+	cont = 1;
+	// recorrido
+	for(i = pos; i<sb.st_size;i++){
+		//recorremos
+		if(lon == strlen(res)){ // Son iguales por su longitud
+
+		    if(arreglo[i] == '	' || arreglo[i] == ' ' || arreglo[i] == '\n' || arreglo[i] == '<' || arreglo[i] == '*' || arreglo[i] == '+' || arreglo[i] == '-'|| arreglo[i] == '['|| arreglo[i] == '{'|| arreglo[i] == '('|| arreglo[i] == ')'|| arreglo[i] == ';'|| arreglo[i] == ',' || arreglo[i] == '"') // revisamos el siguiente valor
+		        cont++;
+		    else lon = 0; //reseteamos la longitud de la palabra
+		} else{
+		    if(res[lon] == arreglo[i]){ // Si son iguales, su tamaño coincide
+		        lon++;
+		    } else lon = 0;
+		}
+	}
+	if(cont > 1) printf("%s se repite %d veces\n",res,cont);	
 }
